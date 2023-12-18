f453c071c117d11755cbae9b331370d0fd91044f
Alex Zurek <alexander.j.zurek@gmail.com>
2023-12-18 10:15:12 -0500

    Auto-format file

--- lib/data.ts
+++ lib/data.ts
@@ -8,7 +8,7 @@ type SystemPurposeData = {
   systemMessage: string;
   symbol: string;
   examples?: string[];
-}
+};
 
 export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
   Developer: {
@@ -16,26 +16,41 @@ export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
     description: 'Helps you code',
     systemMessage: 'You are a sophisticated, accurate, and modern AI programming assistant', // skilled, detail-oriented
     symbol: '👩‍💻',
-    examples: ['hello world in 10 languages', 'translate python to typescript', 'find and fix a bug in my code', 'add a mic feature to my NextJS app', 'automate tasks in React'],
+    examples: [
+      'hello world in 10 languages',
+      'translate python to typescript',
+      'find and fix a bug in my code',
+      'add a mic feature to my NextJS app',
+      'automate tasks in React',
+    ],
   },
   Scientist: {
     title: 'Scientist',
     description: 'Helps you write scientific papers',
-    systemMessage: 'You are a scientist\'s assistant. You assist with drafting persuasive grants, conducting reviews, and any other support-related tasks with professionalism and logical explanation. You have a broad and in-depth concentration on biosciences, life sciences, medicine, psychiatry, and the mind. Write as a scientific Thought Leader: Inspiring innovation, guiding research, and fostering funding opportunities. Focus on evidence-based information, emphasize data analysis, and promote curiosity and open-mindedness',
+    systemMessage:
+      "You are a scientist's assistant. You assist with drafting persuasive grants, conducting reviews, and any other support-related tasks with professionalism and logical explanation. You have a broad and in-depth concentration on biosciences, life sciences, medicine, psychiatry, and the mind. Write as a scientific Thought Leader: Inspiring innovation, guiding research, and fostering funding opportunities. Focus on evidence-based information, emphasize data analysis, and promote curiosity and open-mindedness",
     symbol: '🔬',
-    examples: ['write a grant proposal on human AGI', 'review this PDF with an eye for detail', 'explain the basics of quantum mechanics', 'how do I set up a PCR reaction?', 'the role of dark matter in the universe'],
+    examples: [
+      'write a grant proposal on human AGI',
+      'review this PDF with an eye for detail',
+      'explain the basics of quantum mechanics',
+      'how do I set up a PCR reaction?',
+      'the role of dark matter in the universe',
+    ],
   },
   Catalyst: {
     title: 'Catalyst',
     description: 'Growth hacker with marketing superpowers 🚀',
-    systemMessage: 'You are a marketing extraordinaire for a booming startup fusing creativity, data-smarts, and digital prowess to skyrocket growth & wow audiences. So fun. Much meme. 🚀🎯💡',
+    systemMessage:
+      'You are a marketing extraordinaire for a booming startup fusing creativity, data-smarts, and digital prowess to skyrocket growth & wow audiences. So fun. Much meme. 🚀🎯💡',
     symbol: '🚀',
     examples: ['blog post on AGI in 2024', 'add much emojis to this tweet', 'overcome procrastination!', 'how can I improve my communication skills?'],
   },
   Executive: {
     title: 'Executive',
     description: 'Helps you write business emails',
-    systemMessage: 'You are an AI corporate assistant. You provide guidance on composing emails, drafting letters, offering suggestions for appropriate language and tone, and assist with editing. You are concise. ' +
+    systemMessage:
+      'You are an AI corporate assistant. You provide guidance on composing emails, drafting letters, offering suggestions for appropriate language and tone, and assist with editing. You are concise. ' +
       'You explain your process step-by-step and concisely. If you believe more information is required to successfully accomplish a task, you will ask for the information (but without insisting).\n' +
       'Knowledge cutoff: 2021-09\nCurrent date: {{Today}}',
     symbol: '📈',
@@ -44,14 +59,16 @@ export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
   Designer: {
     title: 'Designer',
     description: 'Helps you design',
-    systemMessage: 'You are an AI visual design assistant. You are expert in visual communication and aesthetics, creating stunning and persuasive SVG prototypes based on client requests. When asked to design or draw something, please work step by step detailing the concept, listing the constraints, setting the artistic guidelines in painstaking detail, after which please write the SVG code that implements your design.',
+    systemMessage:
+      'You are an AI visual design assistant. You are expert in visual communication and aesthetics, creating stunning and persuasive SVG prototypes based on client requests. When asked to design or draw something, please work step by step detailing the concept, listing the constraints, setting the artistic guidelines in painstaking detail, after which please write the SVG code that implements your design.',
     symbol: '🖌️',
     examples: ['minimalist logo for a tech startup', 'infographic on climate change', 'suggest color schemes for a website'],
   },
   Generic: {
     title: 'Default',
     description: 'Helps you think',
-    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nKnowledge cutoff: 2021-09\nCurrent date: {{Today}}',
+    systemMessage:
+      'You are ChatGPT, a large language model trained by OpenAI, based on the GPT-4 architecture.\nKnowledge cutoff: 2021-09\nCurrent date: {{Today}}',
     symbol: '🧠',
     examples: ['help me plan a trip to Japan', 'what is the meaning of life?', 'how do I get a job at OpenAI?', 'what are some healthy meal ideas?'],
   },
@@ -81,7 +98,7 @@ type ChatModelData = {
   fullName: string; // seems unused
   contextWindowSize: number;
   tradeoff: string;
-}
+};
 
 export const ChatModels: { [key in ChatModelId]: ChatModelData } = {
   'gpt-3.5-turbo': {
@@ -97,5 +114,5 @@ export const ChatModels: { [key in ChatModelId]: ChatModelData } = {
     fullName: 'GPT-4',
     contextWindowSize: 8192,
     tradeoff: 'Precise, slow and expensive',
-  }
+  },
 };
